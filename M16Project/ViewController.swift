bb5151e0be90b55b24409feac093f34930ee578e
Stalin <borninfire@yahoo.com>
2023-02-19 19:07:00 +0300

    changed color

--- M16Project/ViewController.swift
+++ M16Project/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = "Pavel"
+        view.backgroundColor = .blue
     }
 
 
